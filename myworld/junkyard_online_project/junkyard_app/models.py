9bcca29c2cc479d3ca802a573d43f7490e63fcf0
J1a2n3s4 <149789153+J1a2n3s4@users.noreply.github.com>
2025-03-13 20:20:03 +0100

    kontakt, dzialajacy product page, wiecej produktow

--- myworld/junkyard_online_project/junkyard_app/models.py
+++ myworld/junkyard_online_project/junkyard_app/models.py
@@ -5,3 +5,7 @@ class product(models.Model):
   description = models.CharField(max_length=1000)
   image_url = models.CharField(max_length=300)
   link = models.CharField(max_length=200)
+
+class kontakt(models.Model):
+  tel = models.CharField(max_length=21)
+  email = models.CharField(max_length=400)
--- myworld/junkyard_online_project/junkyard_app/views.py
+++ myworld/junkyard_online_project/junkyard_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import product
+from .models import product, kontakt
 from django.http import HttpResponse
 from django.template import loader
 
@@ -11,6 +11,11 @@ def main(request):
   return HttpResponse(template.render(content, request))
 
 def product_page(request, name_url):
+  if request.method == "POST":
+    telefon = request.POST.get('telefon')
+    mail = request.POST.get('mail')
+    kontakt(tel = telefon, email = mail).save()
+
   template = loader.get_template('main.html')
   content = {
     "x": product.objects.get(link = name_url)
